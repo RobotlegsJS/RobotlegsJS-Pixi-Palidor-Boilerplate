44ccc2ac873859a3953fe1d69823b8185e696987
RonaldoSetzer <Ronaldo.Setzer@gmail.com>
2017-12-07 10:04:15 -0200

    config webpack to support typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// tslint:disable-next-line:no-reference
+/// <reference path="../node_modules/@robotlegsjs/pixi/definitions/pixi.d.ts" />
+import "reflect-metadata";
+
+class Main {
+    constructor() {
+        console.log("main");
+    }
+}
+const main = new Main();
