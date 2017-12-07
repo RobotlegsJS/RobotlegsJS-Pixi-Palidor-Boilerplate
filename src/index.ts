83292792ebc7b900b50e2a69125ae331db313c15
RonaldoSetzer <Ronaldo.Setzer@gmail.com>
2017-12-07 11:26:59 -0200

    add a full palidor example

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,35 @@
 /// <reference path="../node_modules/@robotlegsjs/pixi/definitions/pixi.d.ts" />
 import "reflect-metadata";
 
+import { Context, MVCSBundle } from "@robotlegsjs/core";
+import { ContextView, PixiBundle } from "@robotlegsjs/pixi";
+import { PalidorPixiExtension } from "@robotlegsjs/pixi-palidor";
+import { CanvasRenderer, Container, WebGLRenderer, autoDetectRenderer } from "pixi.js";
+
+import { ExampleConfig } from "./config/ExampleConfig";
+
 class Main {
+    private stage: Container;
+    private renderer: CanvasRenderer | WebGLRenderer;
+    private context: Context;
+
     constructor() {
-        console.log("main");
+        this.renderer = autoDetectRenderer(400, 600, {});
+        this.stage = new Container();
+        this.context = new Context();
+        this.context
+            .install(MVCSBundle, PixiBundle)
+            .install(PalidorPixiExtension)
+            .configure(new ContextView(this.stage))
+            .configure(ExampleConfig)
+            .initialize();
+
+        document.body.appendChild(this.renderer.view);
+    }
+    public render = () => {
+        this.renderer.render(this.stage);
+        window.requestAnimationFrame(this.render);
     }
 }
 const main = new Main();
+main.render();
---
+++ src/views/RobotlegsView.ts
@@ -0,0 +1,7 @@
+import { AbstractView } from "./AbstractView";
+
+export class RobotlegsView extends AbstractView {
+    constructor() {
+        super("RobotlegsJS", "assets/robotlegsjs.png", 0x007acd, 0x005c9a);
+    }
+}
